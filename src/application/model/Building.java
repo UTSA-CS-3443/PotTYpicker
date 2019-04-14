6ebc2de8eb60b8cd4153064cd6729ea709240fb0
Lee Reese <leereese3@gmail.com>
2019-04-13 23:47:24 -0500

    Created basis for Building class

---
+++ src/application/model/Building.java
@@ -0,0 +1,33 @@
+package application.model;
+
+import java.io.File;
+
+public class Building {
+	private File dir;
+	private String name;
+	
+	public Building(String dirPath) {
+		dir = new File(dirPath);
+		name = dir.getName();
+	}
+	
+	public String toString() {
+		return this.name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public File getDir() {
+		return this.dir;
+	}
+	
+	public void setDir(File dir) {
+		this.dir = dir;
+	}
+}
